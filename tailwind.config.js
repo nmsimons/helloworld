08e4b0b08f1cc67aa1457ceff57afe88d35b6436
Taylor Williams <taylorsw@microsoft.com>
2023-11-07 15:43:24 -0800

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}", // paths to all of your components
-    ],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-      extend: {
-        width: {
-          '300': '300px', // custom width
-        },
-        height: {
-          '300': '300px', // custom height
-        },
-        // You can add more customizations here
-      },
+        extend: {},
     },
     plugins: [],
-  }
+};
